694a026b003042a8c961ac9d50f1810b39728861
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-01-31 22:21:53 +0530

    Feat(Index) Modified index.js

--- static/assets/dist/js/getdata.js
+++ static/assets/dist/js/getdata.js
@@ -3,29 +3,34 @@ const shortAnswer = document.getElementById("textId");
 const LongAnswer = document.getElementById("longAnswer");
 const checkbox = document.getElementById("checkBoxId");
 
+// document.getElementById("newsectionbtn").addEventListener("click",function () {
+//   const question = document.getElementById("question");
+//   const shortAnswer = document.getElementById("textId");
+//   const LongAnswer = document.getElementById("longAnswer");
+//   const checkbox = document.getElementById("checkBoxId");
+//  console.log(question)
 
-document.getElementById("newsectionbtn").addEventListener("click",function () {
-  const question = document.getElementById("question");
-  const shortAnswer = document.getElementById("textId");
-  const LongAnswer = document.getElementById("longAnswer");
-  const checkbox = document.getElementById("checkBoxId");
- console.log(question)
- 
+// });
+
+document.getElementById("newsectionbtn").addEventListener("click", function () {
+  var tile_child = document.getElementById("mainsection").children;
+
+  for (var i = 0; i < tile_child.length; i++) {
+    console.log(tile_child[i]);
+  }
 });
 
-document.getElementById("shortAnswerId").addEventListener("click",function(){
-    console.log("Short answer added")
-})
-
-document.getElementById("ParagraphId").addEventListener("click",function(){
-    console.log("Long answer added")
-})
-
-
-document.getElementById("checkBoxId").addEventListener("click",function(){
-    console.log("checkbox added")
-})
-
-
-
-
+// function func(){
+//     var xml=new XMLHttpRequest();
+//     xml.open("POST","{{url_for(func)}}",true);
+//     xml.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+//     xml.onload=function(){
+//         var dataReply=JSON.parse(this.responseText)
+//         alert(dataReply)
+//     }
+//     document.getElementById("Submit").addEventListener("click",function(){
+//         var tile_child=document.getElementById("mainsection").children
+//         dataSend=JSON.stringify({'data':tile_child})
+//     })
+//     xml.send(dataSend)
+// }
--- static/assets/dist/js/index.js
+++ static/assets/dist/js/index.js
@@ -1,7 +1,5 @@
 //toggle view
 
-
-
 // function myText() {
 //     var x = document.getElementById("text");
 //     if (x.style.display === "none") {
@@ -32,71 +30,122 @@
 //     }
 // }
 
-function toggleText(item){ 
-  parentEl = item.parentNode.parentNode; 
+function toggleText(item) {
+  parentEl = item.parentNode.parentNode;
   targetEl = parentEl.nextSibling.nextSibling.children[0];
   targetCont = parentEl.nextSibling.nextSibling;
 
- // var children = targetCont.childNodes;
- // var array = Array.prototype.slice.call(children);
- // children.forEach(function(item){
- //   item.style.display = "none";
- // });
+  // var children = targetCont.childNodes;
+  // var array = Array.prototype.slice.call(children);
+  // children.forEach(function(item){
+  //   item.style.display = "none";
+  // });
 
- for(var child=targetCont.firstChild; child!==null; child=child.nextElementSibling) {
-   child.style.display = "none";
-}
-
+  for (
+    var child = targetCont.firstChild;
+    child !== null;
+    child = child.nextElementSibling
+  ) {
+    child.style.display = "none";
+  }
 
   if (targetEl.style.display === "none") {
-     targetEl.style.display = "block";
-   } else {
-     targetEl.style.display = "none";
-   }
+    targetEl.style.display = "block";
+    temp = 0;
+  } else {
+    targetEl.style.display = "none";
+  }
 }
 
-function myTextArea(item){ 
-  parentEl = item.parentNode.parentNode; 
+function myTextArea(item) {
+  parentEl = item.parentNode.parentNode;
   targetEl = parentEl.nextSibling.nextSibling.children[1];
   targetCont = parentEl.nextSibling.nextSibling;
 
- for(var child=targetCont.firstChild; child!==null; child=child.nextElementSibling) {
-   child.style.display = "none";
-}
+  for (
+    var child = targetCont.firstChild;
+    child !== null;
+    child = child.nextElementSibling
+  ) {
+    child.style.display = "none";
+  }
 
   if (targetEl.style.display === "none") {
-     targetEl.style.display = "block";
-   } else {
-     targetEl.style.display = "none";
-   }
+    targetEl.style.display = "block";
+    temp = 1;
+  } else {
+    targetEl.style.display = "none";
+  }
 }
 
-function myCheckBox(item){ 
-  parentEl = item.parentNode.parentNode; 
+function myCheckBox(item) {
+  parentEl = item.parentNode.parentNode;
   targetEl = parentEl.nextSibling.nextSibling.children[2];
   targetCont = parentEl.nextSibling.nextSibling;
 
- for(var child=targetCont.firstChild; child!==null; child=child.nextElementSibling) {
-   child.style.display = "none";
-}
+  for (
+    var child = targetCont.firstChild;
+    child !== null;
+    child = child.nextElementSibling
+  ) {
+    child.style.display = "none";
+  }
 
   if (targetEl.style.display === "none") {
-     targetEl.style.display = "block";
-   } else {
-     targetEl.style.display = "none";
-   }
+    targetEl.style.display = "block";
+    temp = 2;
+  } else {
+    targetEl.style.display = "none";
+  }
 }
 
-document.getElementById("newsectionbtn").onclick = function() {
- var tile = document.getElementById("tile");
- var section = document.getElementById("mainsection");
- var clone = tile.cloneNode(true)                                            // Create a clone to avoid overwriting
- var num = document.getElementById("mainsection").childElementCount;         // Count number of children in mainsection
- clone.setAttribute("id","tile"+num);                                        // Set new id as tile + num eg: tile1,tile2,tile3
- section.appendChild(clone);                                                 // Add clone tile to the existing section, don't add sections to tiles
-}
+document.getElementById("newsectionbtn").onclick = function () {
+  var tile = document.getElementById("tile");
+  var section = document.getElementById("mainsection");
+
+  var clone = tile.cloneNode(true); // Create a clone to avoid overwriting
+  var num = document.getElementById("mainsection").childElementCount; // Count number of children in mainsection
+  clone.setAttribute("id", "tile" + num);
+  // Set new id as tile + num eg: tile1,tile2,tile3
+  section.appendChild(clone);
+
+  // Add clone tile to the existing section, don't add sections to tiles
+};
+
+// document.getElementById("Submit").onclick=function(){
+
+//   var num = document.getElementById("mainsection").childElementCount;
+//   var shortAnswer=document.getElementById("textId");
+//   var longAnswer=document.getElementById("longAnswer");
+//   var checkBox=document.getElementById("checkBoxId");
+
+//   for(var i=1;i<=num;i++){
+//     // var shortAnswer=document.getElementById("textId");
+//     // var longAnswer=document.getElementById("longAnswer");
+//     // var checkBox=document.getElementById("checkBoxId");
+//     var section=document.getElementById("tile"+i)
+//     shortAnswer.setAttribute("id","textId"+i)
+//     longAnswer.setAttribute("id","longAnswer"+i)
+//     checkBox.setAttribute("id","checkBoxId"+i)
+//    }
+
+// }
 
 // Always include script tags after the document body and css files before the document body
 
 // removing div on buttton click
 
+var temp = 0;
+
+function select() {
+  var num = document.getElementById("mainsection").childElementCount;
+
+  for (var i = 0; i < num; i++) {
+    var ele = $("#tile" + i).find("#answers");
+    // console.log(ele);
+    $(".answers:visible").each(console.log("Visible"));
+
+    var res = $("ele > :visible");
+    console.log(res);
+  }
+}
